6cdc2196bd166371b622ccc6e19ebb726ec67883
chalimbu <sebastian@DESKTOP-VQ81INF.localdomain>
2021-04-03 12:42:14 -0500

    ability to consult post with graphql added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
 //import { Post } from "./entities/Post";
@@ -6,18 +7,20 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig)
     await orm.getMigrator().up()
-    
+
     const app = express()
 
-    const apolloServer=new ApolloServer({
+    const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false,
         }),
+        context: () => ({ em: orm.em })
     });
 
     apolloServer.applyMiddleware({ app });
