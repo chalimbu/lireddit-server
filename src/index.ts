cbf91db5f46b82a51c2f466e244d7c3125d9e0bd
chalimbu <sebitaszapata12@gmail.com>
2021-04-16 19:26:52 -0500

    starting to add redis

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,53 @@
-import "reflect-metadata"
-import { MikroORM } from "@mikro-orm/core"
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-//import { Post } from "./entities/Post";
+
 import microConfig from "./mikro-orm.config";
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { HelloResolver } from './resolvers/hello'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from 'connect-redis'
+
+
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig)
-    await orm.getMigrator().up()
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-    const app = express()
+  const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver,UserResolver],
-            validate: false,
-        }),
-        context: () => ({ em: orm.em })
-    });
 
-    apolloServer.applyMiddleware({ app });
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
 
-    app.listen(4000, () => {
-        console.log('server started on localhost:4000')
-    })
+  app.use(
+      session({
+          name: 'qid',
+          store: new RedisStore({client:redisClient}),
+          secret: "adsfasdfrwerwec",
+          resave: false,
+      })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
-
-console.log("hello world..")
-main()
+console.log("hello world..");
+main();
